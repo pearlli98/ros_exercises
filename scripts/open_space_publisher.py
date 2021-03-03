773d12418c9cbbdcef5bf6c34bf237b07d1c70ed
Pearl <pearlli@mit.edu>
2021-03-03 01:08:19 -0500

    add OpenSpace.msg

--- scripts/open_space_publisher.py
+++ scripts/open_space_publisher.py
@@ -4,18 +4,23 @@ import rospy
 import math
 from std_msgs.msg import Float32
 from sensor_msgs.msg import LaserScan
+from ros_exercises.msg import OpenSpace
 
 rospy.init_node('open_space_publisher', anonymous = False)
-pub1 = rospy.Publisher('open_space/distance', Float32, queue_size = 20)
-pub2 = rospy.Publisher('open_space/angle', Float32, queue_size = 20)
+# pub1 = rospy.Publisher('open_space/distance', Float32, queue_size = 20)
+# pub2 = rospy.Publisher('open_space/angle', Float32, queue_size = 20)
+pub = rospy.Publisher('open_space', OpenSpace, queue_size=20)
 
 def callback(data):
     m = max(data.ranges)
     a = data.angle_max + data.ranges.index(m) * data.angle_increment
-    pub1.publish(Float32(m))
-    pub2.publish(Float32(a))
-    rospy.loginfo(m)
-    rospy.loginfo(a)
+    os = OpenSpace()
+    os.angle = a
+    os.distance = m
+    pub.publish(os)
+    # pub1.publish(Float32(m))
+    # pub2.publish(Float32(a))
+    rospy.loginfo(data.ranges.index(m))
 
 def open_space():
     sub = rospy.Subscriber('fake_scan', LaserScan, callback)
